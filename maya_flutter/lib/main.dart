f65ec3f42fc632d91cf4e6730e92103854fa4cf7
bun133 <bun133.work@gmail.com>
2022-06-19 13:50:12 +0900

    Add FlutterFire

--- maya_flutter/lib/main.dart
+++ maya_flutter/lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:maya_flutter/firebase_options.dart';
 import 'package:maya_flutter/messages.i18n.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MayaApp());
 }
 
